5e8ef978aea6d315b1c464be129068e3bc6023ea
Sílvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-05-19 14:24:10 -0300

    Adicionando classe e pacotes repositorie

---
+++ codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/repositories/PessoaRepository.java
@@ -0,0 +1,11 @@
+package edu.ifes.ci.si.les.slol.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.ifes.ci.si.les.slol.model.*;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+
+}
