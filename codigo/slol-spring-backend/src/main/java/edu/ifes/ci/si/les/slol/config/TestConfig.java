8fe5abd97ea15b5f6796f6743a65feb81a3c6735
Sílvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-05-19 14:30:38 -0300

    Adicionando pacote config e classes de configuracao

---
+++ codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/config/TestConfig.java
@@ -0,0 +1,25 @@
+package edu.ifes.ci.si.les.slol.config;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import edu.ifes.ci.si.les.slol.services.*;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private _DBService dbService;
+	
+	@Bean
+	public boolean instatiateDatabase() throws ParseException, IOException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
