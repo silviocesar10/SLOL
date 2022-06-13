46d318e3b470cdd89ea6cb65b3962d6913ed2bb2
Sílvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-06-13 13:09:30 -0300

    Atualizando a class TestConfig

--- codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/config/TestConfig.java
+++ codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/config/TestConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 import edu.ifes.ci.si.les.slol.services.*;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class TestConfig {
 	
 	@Autowired
