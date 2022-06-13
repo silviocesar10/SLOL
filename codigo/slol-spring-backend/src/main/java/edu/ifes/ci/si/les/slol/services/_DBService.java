e8635041ec6fd8b0e3cf07cc2e4e322f5cda6160
Sílvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-06-13 13:12:12 -0300

    Atualizando a class service _DBService para acomaodar mudancas necessarias ao modelo

--- codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/services/_DBService.java
+++ codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/services/_DBService.java
@@ -35,8 +35,8 @@ public class _DBService {
 		Livro lvr1 = new Livro(01, "Harry Potter e a Pedra Filosofal", "Harry Potter e a Pedra Filosofal é o primeiro dos sete livros da série de fantasia Harry Potter, escrita por J. K. Rowling.","972-23-2533-7", "J. K. Rowling", sdf.parse("1999-10-14"),catLvr2);
 		Livro lvr2 = new Livro(02, "O Iluminado", "The Shining é um romance de horror do escritor estadunidense Stephen King.","9788573022035", "Stephen King", sdf.parse("1977-01-28"),catLvr1);
 		
-		Reserva rsv1 = new Reserva(01, sdf.parse("2022-05-18"), sdf.parse("2022-05-25"), 0.0F, 12,cl1, lvr1);
-		Reserva rsv2 = new Reserva(02, sdf.parse("2022-05-18"), sdf.parse("2022-06-12"), 0.0F, 12,cl2, lvr2);
+		Reserva rsv1 = new Reserva(01, sdf.parse("2022-05-18"), sdf.parse("2022-05-25"),sdf.parse("2022-06-24"), 0.0F, 12,cl1, lvr1);
+		Reserva rsv2 = new Reserva(02, sdf.parse("2022-05-18"), sdf.parse("2022-06-12"),sdf.parse("2022-06-24"), 0.0F, 12,cl2, lvr2);
 		
 		categoriaLivroRepository.save(catLvr1);
 		categoriaLivroRepository.save(catLvr2);
