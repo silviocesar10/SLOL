9758e65ebbb841a1cc9e9b0f1dc338439d85c2d9
Sílvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-07-03 11:06:17 -0300

    Atualizando a classe ReservaRepository para conter as atuais querys das regras de negocio da aplicacao

--- codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/repositories/ReservaRepository.java
+++ codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/repositories/ReservaRepository.java
@@ -1,12 +1,12 @@
 package edu.ifes.ci.si.les.slol.repositories;
 
 import java.util.Collection;
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.Query;
-//import java.util.Date;
-import edu.ifes.ci.si.les.slol.model.Livro;
 
 import edu.ifes.ci.si.les.slol.model.*;
 
@@ -14,14 +14,17 @@ import edu.ifes.ci.si.les.slol.model.*;
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 		
 	@Transactional(readOnly = true)
-    @Query(value = "SELECT * FROM reserva rsv WHERE  rsv.id_livro = ?1 AND ?2 <= rsv.data_entrega AND ?2 >= rsv.data_retirada;\n" , nativeQuery = true)
-    public Collection<Reserva> findByReservaConflict(Livro lvr, String data);
+    //@Query(value = "SELECT * FROM RESERVA WHERE RESERVA.ID_LIVRO = ?1 AND (?2 >= RESERVA.DATA_RETIRADA AND ?2 <= RESERVA.DATA_ENTREGA) OR (?3 >= RESERVA.DATA_RETIRADA AND ?3 <= RESERVA.DATA_ENTREGA)" , nativeQuery = true)
+	//@Query(value = "SELECT * FROM reserva" , nativeQuery = true)
+	@Query(value = "SELECT * FROM RESERVA rsv WHERE rsv.ID_LIVRO = ?1 AND (?2 >= rsv.DATA_RETIRADA AND ?3 <= rsv.DATA_ENTREGA) OR (?2 >= rsv.DATA_RETIRADA AND ?3 <= rsv.DATA_ENTREGA)" , nativeQuery = true)
+    public Collection<Reserva> findByReservaConflict(Integer id, Date retirada, Date entrega);
 
-    @Transactional(readOnly = true)
-    @Query(value = "select COUTN(*) FROM reservas rsv WHERE rsv.data_reserva = ?1 AND rsv.data_reserva = ?2", nativeQuery = true)
-	public Integer findQuantidadesReservasOfClientesByPeriodo(String inicio, String termino);
+    //@Transactional(readOnly = true)
+    //@Query(value = "select CLIENTE.NOME as nome, count(VALOR) as quantidade from RESERVA inner join CLIENTE on RESERVA.ID_PESSOA = CLIENTE.ID_PESSOA where DATA_RESERVA > '2022-01-01' and DATA_RESERVA < '2022-12-31' group by CLIENTE.NOME", nativeQuery = true)
+	//public Collection<?> findQuantidadesReservasOfClientesByPeriodo(String inicio, String termino);
     
     @Transactional(readOnly = true)
-    @Query(value = "SELECT * FROM reservas rsv WHERE rsv,id_pessoa = ?1 AND rsv.data_reserva = ?2 AND rsv.data_reserva = ?3", nativeQuery = true)
-    public Collection<Reserva> findByClienteAndPeriodo(Integer idCliente, String inicio, String termino);
+    //@Query(value = "SELECT * FROM reserva rsv WHERE rsv.id_pessoa = ?1 AND rsv.data_reserva = ?2 AND rsv.data_reserva = ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM reserva rsv WHERE rsv.id_pessoa = ?1 AND extract(month FROM rsv.data_reserva) = extract(month FROM (SELECT CURRENT_DATE)) AND extract(year FROM rsv.data_reserva) = extract(year FROM (SELECT CURRENT_DATE))", nativeQuery = true)
+    public Collection<Reserva> findByClienteAndPeriodoInMonth(Integer idCliente);
 }
