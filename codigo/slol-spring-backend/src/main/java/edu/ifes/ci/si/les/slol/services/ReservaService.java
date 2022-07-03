3a5315d4e41850ac3bc65c77c9914c1140fccf64
Sílvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-07-03 11:07:23 -0300

    Atualizando a classe ReservaService para fazer as corretas verificacoes das regras de negocio atraves do repository

--- codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/services/ReservaService.java
+++ codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/services/ReservaService.java
@@ -2,16 +2,13 @@ package edu.ifes.ci.si.les.slol.services;
 
 import java.util.Collection;
 import java.util.NoSuchElementException;
-
-
+import edu.ifes.ci.si.les.slol.model.Cliente;
+import edu.ifes.ci.si.les.slol.model.Livro;
 import edu.ifes.ci.si.les.slol.model.Reserva;
 import edu.ifes.ci.si.les.slol.repositories.ReservaRepository;
-//import edu.ifes.ci.si.les.slol.model.Cliente;
-import edu.ifes.ci.si.les.slol.model.Livro;
 import edu.ifes.ci.si.les.slol.services.exceptions.DataIntegrityException;
 import edu.ifes.ci.si.les.slol.services.exceptions.ObjectNotFoundException;
 import edu.ifes.ci.si.les.slol.services.exceptions.BusinessRuleException;
-import edu.ifes.ci.si.les.slol.utils.LocalDateOperations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -23,6 +20,12 @@ public class ReservaService {
 	
 	@Autowired
 	private ReservaRepository repository;
+	
+	@Autowired
+	private LivroService livroService;
+	
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Reserva findById(Integer id) {
     	try {
@@ -38,11 +41,37 @@ public class ReservaService {
     }
     
     public Reserva insert(Reserva obj) {
-        try {
-        	if(verificarRegrasDeNegocio1(obj)) {
-	        	obj.setIdReserva(null);
-	        	return repository.save(obj);
-        	}
+    	Livro livro = livroService.findById(obj.getLivro().getIdLivro());
+    	Cliente cliente = clienteService.findById(obj.getCliente().getIdPessoa());
+    	
+    	obj.setLivro(livro);
+    	obj.setCliente(cliente);
+    	
+    	float tmp = 0.0F;
+    	try {
+    		if(verificarRegrasDeNegocio1(obj)){
+    			if(!obj.getCliente().getTemPlano()){
+            		obj.setIdReserva(null);
+    	        	if(!verificarRegrasDeNegocio2(obj))
+    	        	{
+    	        		//aqui eu estaria tratando a segunda regra de negocio pra gerar o desconto
+    	        		//de 3,90 a cada 3 dias para 3 reais a cada tres dias
+    	        		tmp = calcularValor(obj.getPeriodoEmprestimo(), 3.9F);
+    	        		obj.setValor(tmp);
+    	        	}else 
+    	        	{
+    	        		tmp = calcularValor(obj.getPeriodoEmprestimo(), 3.00F);
+    	        		obj.setValor(tmp);
+    	        	}
+    	        	System.out.println("reserva " + obj);
+    	        	return repository.save(obj);
+            	}else 
+            	{
+            		obj.setIdReserva(null);
+            		obj.setValor(0.0F);
+            		return repository.save(obj);
+            	}
+    		}
         } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityException("Campo(s) obrigatório(s) da Reserva não foi(foram) preenchido(s): Cliente ou Livro");
         }
@@ -66,39 +95,55 @@ public class ReservaService {
             throw new DataIntegrityException("Não é possível excluir esta Reserva!");
         }
     }
-
-    public Collection<Reserva> findByClienteAndPeriodo(Integer idCliente, String inicio, String termino) {
-        return repository.findByClienteAndPeriodo(idCliente, inicio, termino);
-    }
-
-    public Integer findQuantidadesReservasOfClientesByPeriodo(String inicio, String termino){
-       return repository.findQuantidadesReservasOfClientesByPeriodo(inicio, termino);
-    }
-    
-    public Collection<Reserva> findByReservaConflict(Livro lvr, String data)
-    {
-    	return repository.findByReservaConflict(lvr, data);
-    }
    
     
     //mplementando as regras de negócio relacionadas ao processo de negócio Reserva
  	//Regra de Negócio 1: Cliente não pode ter multas não pagas
  	public boolean verificarRegrasDeNegocio1(Reserva obj) {
- 		LocalDateOperations operator = new LocalDateOperations(); 
- 		int periodo =obj.getPeriodoEmprestimo();
- 		String data = obj.getDataRetirada().toString();
- 		for(int i =periodo; i > 0; i--)
- 		{
- 			data = operator.addNDay(data, i);
- 			Collection<Reserva> lst = findByReservaConflict(obj.getLivro(), data);
+ 		Collection<Reserva> lst = null;
+ 			lst = repository.findByReservaConflict(obj.getLivro().getIdLivro(), obj.getDataRetirada(), obj.getDataEntrega());
  			if(!lst.isEmpty())
  			{
  				throw new BusinessRuleException("O periodo de reserva que compreende a data de retirada e a data de entrega esta em conflito, selecione outro periodo");
  				
  			}
- 		}
  		return true;
  	}
  	
+
+ 	public boolean verificarRegrasDeNegocio2(Reserva obj)
+ 	{
+ 			//aqui eu faco a conversao das datas para o formato correto para verificar
+ 			//se o cliente se encaixa no requisito de no minimo de 3 reservas no mes
+ 			Collection<Reserva> lst = repository.findByClienteAndPeriodoInMonth(obj.getCliente().getIdPessoa());
+ 			if(!lst.isEmpty() && lst.size() >= 3)
+ 			{
+ 				return true;
+ 			}else
+ 			{
+ 				return false;
+ 			}
+ 		}
+ 		
  	
+ 	public float calcularValor(int periodo, float valor)
+ 	{
+ 		float res = 0.0F;
+		switch(periodo){
+			case 3:
+				res = valor;
+				break;
+			case 6:
+				res = 2 * valor;
+				break;
+			case 9:
+				res = 3 * valor;
+				break;
+			case 12:
+				res = 4 * valor;
+				break;
+			
+		}
+ 		return res;
+ 	}
 }
