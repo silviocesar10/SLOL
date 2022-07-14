b90e468047e0cb5c049ebf46a6c7fca00ded7320
Sílvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-07-14 15:01:33 -0300

    Atualizando a classe ReservaRepository para conter as requiscoes de relatorios

--- codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/repositories/ReservaRepository.java
+++ codigo/slol-spring-backend/src/main/java/edu/ifes/ci/si/les/slol/repositories/ReservaRepository.java
@@ -14,14 +14,16 @@ import edu.ifes.ci.si.les.slol.model.*;
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
 		
 	@Transactional(readOnly = true)
-    //@Query(value = "SELECT * FROM RESERVA WHERE RESERVA.ID_LIVRO = ?1 AND (?2 >= RESERVA.DATA_RETIRADA AND ?2 <= RESERVA.DATA_ENTREGA) OR (?3 >= RESERVA.DATA_RETIRADA AND ?3 <= RESERVA.DATA_ENTREGA)" , nativeQuery = true)
-	//@Query(value = "SELECT * FROM reserva" , nativeQuery = true)
 	@Query(value = "SELECT * FROM RESERVA rsv WHERE rsv.ID_LIVRO = ?1 AND (?2 >= rsv.DATA_RETIRADA AND ?3 <= rsv.DATA_ENTREGA) OR (?2 >= rsv.DATA_RETIRADA AND ?3 <= rsv.DATA_ENTREGA)" , nativeQuery = true)
     public Collection<Reserva> findByReservaConflict(Integer id, Date retirada, Date entrega);
 
-    //@Transactional(readOnly = true)
-    //@Query(value = "select CLIENTE.NOME as nome, count(VALOR) as quantidade from RESERVA inner join CLIENTE on RESERVA.ID_PESSOA = CLIENTE.ID_PESSOA where DATA_RESERVA > '2022-01-01' and DATA_RESERVA < '2022-12-31' group by CLIENTE.NOME", nativeQuery = true)
-	//public Collection<?> findQuantidadesReservasOfClientesByPeriodo(String inicio, String termino);
+    @Transactional(readOnly = true)
+    @Query(value = "select c.nome as nome, count(r.valor) as quantidade, l.autor, l.titulo_livro from reserva r, livro l, cliente c where r.id_pessoa = c.id_pessoa and r.id_livro = l.id_livro and DATA_RESERVA > ?1 and DATA_RESERVA < ?2 group by c.nome , l.autor, l.titulo_livro;", nativeQuery = true)
+	public Collection<?> findQuantidadesReservasOfClientesByPeriodo(Date inicio, Date termino);
+    
+    @Transactional(readOnly = true)
+    @Query(value = "select c.nome as nome, count(r.valor) as quantidade, l.autor, l.titulo_livro from reserva r, livro l, cliente c where r.id_pessoa = c.id_pessoa group by c.nome , l.autor, l.titulo_livro", nativeQuery = true)
+	public Collection<?> findQuantidadesReservasOfClientes();
     
     @Transactional(readOnly = true)
     //@Query(value = "SELECT * FROM reserva rsv WHERE rsv.id_pessoa = ?1 AND rsv.data_reserva = ?2 AND rsv.data_reserva = ?3", nativeQuery = true)
